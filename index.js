fb26db1e698e8dac5042fbd60875b2b6e1bd7175
AJ Jalusi <ajalusi26@gmail.com>
2021-11-29 12:46:21 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(){
+    const name = [ 'Lisa', 'Kaitlin', 'Jan' ];
+    const action = ['surprise'];
+    const messages = [];
+    
+    for ( let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${action} gift!`)
+        }
+        return messages
+}
+
+function countDown(){
+   for (let i = 10; i >= 0; i--){
+       console.log(i);
+   }
+}
